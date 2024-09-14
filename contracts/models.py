0b271fba8372caf4d8a76d40c4b637433463ddff
ok-pennywise <souryadeeptasaha03@gmail.com>
2024-09-14 12:30:24 +0530

    Added contract

--- contracts/models.py
+++ contracts/models.py
@@ -1,9 +1,16 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.db import models
 
 
 from model_utils import aware_utcnow, generate_id
+from products.models import Product
 from users.models import User
+
+from django.core.exceptions import ValidationError
+
+
+def _default_contract_end_date() -> datetime:
+    return aware_utcnow() + timedelta(days=365)
 
 
 # Create your models here.
@@ -28,28 +35,54 @@ class Contract(models.Model):
         editable=False,
     )
 
-    quantity: str = models.IntegerField(default=0)
-    description: str = models.TextField(blank=True, null=True)
+    product: Product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
-    price: float = models.DecimalField(max_digits=10, decimal_places=2)
-
-    start_date = models.DateTimeField(default=aware_utcnow)  # Contract start date
-    end_date = models.DateTimeField(null=True, blank=True)
+    start_date: datetime = models.DateTimeField(
+        default=aware_utcnow
+    )  # Contract start date
+    end_date = models.DateTimeField(default=_default_contract_end_date)
 
     payment_terms: str = models.TextField(
         blank=True, null=True
     )  # Details of payment terms
     delivery_terms: str = models.TextField(blank=True, null=True)
 
+    UPI_PAYMENT: str = "upi"
+    COD_PAYMENT: str = "cod"
+    CREDIT_PAYMENT: str = "credit"
+    DEBIT_PAYMENT: str = "debit"
+
     PAYMENT_METHOD_CHOICES: tuple[tuple[str, str]] = (
-        ("upi", "UPI"),
-        ("cod", "Cash on delivery"),
-        ("credit", "Credit card"),
-        ("debit", "Debit card"),
+        (UPI_PAYMENT, "UPI"),
+        (COD_PAYMENT, "Cash on delivery"),
+        (CREDIT_PAYMENT, "Credit card"),
+        (DEBIT_PAYMENT, "Debit card"),
     )
 
     payment_method: str = models.CharField(
         max_length=10, choices=PAYMENT_METHOD_CHOICES, default="upi"
     )
 
-    created_on: datetime = models.DateTimeField(default=aware_utcnow, editable=False)
+    quantity: int = models.PositiveIntegerField()
+    price_per_unit: float = models.DecimalField(max_digits=10, decimal_places=2)
+    total_price: float = models.DecimalField(max_digits=12, decimal_places=2)
+
+    discount: float = models.DecimalField(max_digits=3, decimal_places=2, default=0)
+
+    terms_and_conditions: str = models.TextField()
+
+    is_active: bool = models.BooleanField(default=True)
+
+    def clean(self) -> None:
+        if self.product.max_quantity < self.quantity < self.product.min_quantity:
+            raise ValidationError(
+                f"Minimum of {self.product.min_quantity} and at max {self.product.max_quantity}"
+            )
+        return super().clean()
+
+    def save(self, *a, **kw) -> None:
+        self.price_per_unit = self.product.price
+        self.total_price = self.price_per_unit * self.quantity
+        if self.discount > 0:
+            self.total_price(self.total_price / 100) * self.discount
+        super().save(*a, **kw)
