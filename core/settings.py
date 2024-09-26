04ec9b9a5edad46387c2a6e44c1b4153d49f6e85
ok-pennywise <souryadeeptasaha03@gmail.com>
2024-09-26 22:26:46 +0530

    B

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-47nn1g3!a59p4v(j-iyv8_h8(=^ey0o4a@@m)5pt$wg9$x!a9c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 AUTH_USER_MODEL = "users.User"
 
